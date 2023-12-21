57244f5884f10fbb38a3476f97a191206449d172
T0NY24 <anperezcue@uide.edu.ec>
2023-12-21 18:20:18 -0500

    respaldo3

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Examen1
+¿Qué hace este proyecto? 
+En este proyecto, puedes ingresar información sobre una persona, como su nombre, apellidos, carrera y edad. 
+
+¿Cómo usar este proyecto? 
+Ejecución del Proyecto: Navega hasta el directorio del proyecto y ejecuta flutter run en tu terminal para iniciar la aplicación.
+
+Ingresar Datos: Una vez que la aplicación esté en funcionamiento, presiona el botón flotante con el icono de suma (+) para ingresar información de una persona. Se abrirá un diálogo donde podrás ingresar los datos solicitados.
+
+Visualización de Datos: Después de ingresar la información, se mostrará la información de la persona en la pantalla principal. Esta información está contenida en un contenedor con un borde azul y texto de color azul para destacarla.
+
+Validación de Edad: La aplicación valida que la edad ingresada esté dentro del rango aceptable (entre 0 y 150 años). Si intentas ingresar una edad fuera de este rango, se mostrará un mensaje de error.
+
+Personalización Adicional: Si deseas agregar más funcionalidades o personalizar la interfaz, ¡siéntete libre de hacerlo! Este proyecto sirve como una base simple para expandir y experimentar con Flutter.
+
+Abre la aplicación en tu computadora o teléfono.
+Presiona el botón con un signo más (+) para ingresar información de una persona. Se abrirá una ventana para que escribas su nombre, apellidos, carrera y edad.
+Después de escribir la información, presiona el botón "Aceptar".
+
+Y aparecera la información de la persona que ingresaste.
--- examen/lib/main.dart
+++ examen/lib/main.dart
@@ -4,16 +4,32 @@ void main() {
   runApp(const MyApp());
 }
 
+// Creamos una clase llamada "Persona" para almacenar información sobre una persona.
 class Persona {
   late String nombre;
   late String apellidos;
   late String carrera;
+  late int edad;
 
-  Persona(this.nombre, this.apellidos, this.carrera);
+  // Creamos un constructor para inicializar una Persona con nombre, apellidos, carrera y edad.
+  Persona(this.nombre, this.apellidos, this.carrera, this.edad);
 
+  // Creamos un método de fábrica (factory method) para crear una Persona con edad verificada.
+  factory Persona.conEdad(
+      String nombre, String apellidos, String carrera, int edad) {
+    // Verificamos que la edad esté entre 0 y 150 años.
+    if (edad < 0 || edad > 150) {
+      throw ArgumentError('La edad debe estar entre 0 y 150 años.');
+    }
+
+    // Si la edad está dentro del rango, creamos y devolvemos una Persona.
+    return Persona(nombre, apellidos, carrera, edad);
+  }
+
+  // Creamos un método para mostrar la información de la Persona en formato de texto.
   @override
   String toString() {
-    return 'Nombre: $nombre\nApellidos: $apellidos\nCarrera: $carrera\n';
+    return 'Nombre: $nombre\nApellidos: $apellidos\nCarrera: $carrera\nEdad: $edad años\n';
   }
 }
 
@@ -22,41 +38,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Persona nuevaPersona = obtenerInformacionPersona();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page', persona: nuevaPersona),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-
-  Persona obtenerInformacionPersona() {
-    String nombre =
-        'John'; // Valor predeterminado si el usuario no ingresa datos
-    String apellidos =
-        'Doe'; // Valor predeterminado si el usuario no ingresa datos
-    String carrera =
-        'Informática'; // Valor predeterminado si el usuario no ingresa datos
-
-    // Solicitar al usuario que ingrese los datos para crear una persona
-    // Los datos ingresados por el usuario reemplazarán los valores predeterminados
-    // cuando se introduzcan.
-    // Puedes usar widgets de diálogo o de formulario para esto, pero aquí se usan valores predeterminados.
-    // Supondré que estás recopilando esta información de alguna manera, por ejemplo, mediante widgets TextFormField.
-
-    return Persona(nombre, apellidos, carrera);
   }
 }
 
 class MyHomePage extends StatefulWidget {
   final String title;
-  final Persona persona;
 
-  const MyHomePage({Key? key, required this.title, required this.persona})
-      : super(key: key);
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -64,11 +60,72 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  Persona? nuevaPersona; // Creamos una variable para almacenar una Persona.
 
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
+  }
+
+  // Método para mostrar un cuadro de diálogo para ingresar los datos de la Persona.
+  Future<void> _mostrarDialogo() async {
+    // Creamos controladores para obtener los datos ingresados en los campos de texto.
+    TextEditingController nombreController = TextEditingController();
+    TextEditingController apellidosController = TextEditingController();
+    TextEditingController carreraController = TextEditingController();
+    TextEditingController edadController = TextEditingController();
+
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Ingresa tus datos'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                // Campos de texto para ingresar nombre, apellidos, carrera y edad.
+                TextFormField(
+                  controller: nombreController,
+                  decoration: const InputDecoration(labelText: 'Nombre'),
+                ),
+                TextFormField(
+                  controller: apellidosController,
+                  decoration: const InputDecoration(labelText: 'Apellidos'),
+                ),
+                TextFormField(
+                  controller: carreraController,
+                  decoration: const InputDecoration(labelText: 'Carrera'),
+                ),
+                TextFormField(
+                  controller: edadController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: 'Edad'),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            // Botón de aceptar que guarda la nueva Persona con los datos ingresados.
+            TextButton(
+              child: const Text('Aceptar'),
+              onPressed: () {
+                setState(() {
+                  // Creamos una nueva Persona con los datos ingresados.
+                  nuevaPersona = Persona.conEdad(
+                    nombreController.text,
+                    apellidosController.text,
+                    carreraController.text,
+                    int.parse(edadController.text),
+                  );
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -83,24 +140,36 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'You have pushed the button this many times:', // Mensaje de contador.
             ),
             Text(
-              '$_counter',
+              '$_counter', // Muestra el contador.
               style: Theme.of(context).textTheme.headline6,
             ),
-            Text(
-              'Persona:',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            Text(
-              widget.persona.toString(),
-            ),
+            if (nuevaPersona != null) ...[
+              Container(
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text(
+                  nuevaPersona!
+                      .toString(), // Muestra la información de la Persona.
+                  style: TextStyle(color: Colors.blue),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          _mostrarDialogo(); // Muestra el diálogo para ingresar datos de la Persona.
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
